0700021870d1353ae378d7599238b2a23d71b4d5
Walt Ritscher <waltritscher@hotmail.com>
2017-03-16 01:18:50 -0700

    Add DinnerReservation Model and DinnerReservationdialog

---
+++ DinnerBot/Dialogs/DinnerReservationDialog.cs
@@ -0,0 +1,41 @@
+﻿using DinnerBot.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DinnerBot.Dialogs
+{
+	public class DinnerReservationDialog
+	{
+
+		public static readonly IDialog<string> dialog = Chain.PostToChain()
+			.Select(m => m.Text)
+			.Switch(
+						new RegexCase<IDialog<string>>(new Regex("^hi", RegexOptions.IgnoreCase),
+				(context, text) =>
+				{
+					return Chain.ContinueWith(new GreetingDialog(), AfterGreetingContinuation);
+				}),
+			new DefaultCase<string, IDialog<string>>((context, text) =>
+			{
+				return Chain.ContinueWith(FormDialog.FromForm(DinnerReservation.BuildForm,
+				FormOptions.PromptInStart), AfterGreetingContinuation);
+			}))
+			.Unwrap()
+			.PostToUser();
+
+		private async static Task<IDialog<string>> AfterGreetingContinuation(IBotContext context, IAwaitable<object> item)
+		{
+			var token = await item;
+			var userName = "User";
+			context.UserData.TryGetValue<string>("UserName", out userName);
+			return Chain.Return($"Thank you for using Dinner Bot.");
+		}
+	}
+}
+
---
+++ DinnerBot/Models/DinnerReservation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DinnerBot.Models
+{
+	[Serializable]
+	public class DinnerReservation
+	{
+		// add a property for each dialog choice in the bot.
+		// Bot Frameworks asks the questions in the order
+		// declared in the class.
+
+		public string VenueName { get; set; }
+		public int? PersonCount { get; set; }
+		public DateTime? ReservationDay { get; set; }
+		public DinnerTimeOptions DinnerTime { get; set; }
+		public List<ExtraOptions> Extras { get; set; }
+		public static IForm<DinnerReservation> BuildForm()
+		{
+			return new FormBuilder<DinnerReservation>()
+				.Message("Welcome to the Dinner Bot.  Let's get started.")
+				.Build();
+
+			  
+		}
+
+		public enum ExtraOptions {
+			Flowers,
+			OutsideTable,
+			WatersideTable
+		}
+
+		public enum DinnerTimeOptions {
+			Breakfast,
+			Lunch,
+			Dinner,
+			LateDinner
+		}
+	}
+}
